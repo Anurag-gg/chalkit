dfafa9c9ea3d6ac217067eba5b5b090bc09f6699
Anurag-gg <dsanurag520@gmail.com>
2022-05-26 21:42:16 +0530

    added read only

--- chalkit/editor.py
+++ chalkit/editor.py
@@ -3,10 +3,10 @@ import subprocess
 import platform
 import typer
 import sys
-from git_commit import git_commit
+from chalkit.git_commit import git_commit
 
 
-def editor(path):
+def editor(path, view_only):
     rpath = os.path.join(path, "README.md")
     gpath = os.path.join(path, ".git")
 
@@ -18,20 +18,36 @@ def editor(path):
         typer.secho(f"Error:Git not found at {gpath}", fg=typer.colors.BRIGHT_RED)
         sys.exit()
 
-    typer.secho("Enter the commit message\n> ", fg=typer.colors.BRIGHT_GREEN, nl=False)
-    commit_message = input()
-    if platform.system() == "Windows":
-        nano_path = r"C:\Program Files\Git\usr\bin\nano.exe"
-        if not os.path.isfile(nano_path):
-            typer.secho(
-                f"Make sure nano is installed on correct path:{nano_path}",
-                fg=typer.colors.BRIGHT_RED,
-            )
-            sys.exit()
-        subprocess.call((nano_path, rpath))
-    else:
-        subprocess.call(("nano", rpath))
+    if not view_only:
+        typer.secho(
+            "Enter the commit message\n> ", fg=typer.colors.BRIGHT_GREEN, nl=False
+        )
+        commit_message = input()
+        if platform.system() == "Windows":
+            nano_path = r"C:\Program Files\Git\usr\bin\nano.exe"
+            if not os.path.isfile(nano_path):
+                typer.secho(
+                    f"Make sure nano is installed on correct path:{nano_path}",
+                    fg=typer.colors.BRIGHT_RED,
+                )
+                sys.exit()
+            subprocess.call((nano_path, rpath))
+        else:
+            subprocess.call(("nano", rpath))
 
-    result = git_commit(commit_message, path)
-    if result:
-        typer.secho(f"Error: {result}", fg=typer.colors.BRIGHT_RED)
+        result = git_commit(commit_message, path)
+        if result:
+            typer.secho(f"Error: {result}", fg=typer.colors.BRIGHT_RED)
+
+    else:
+        if platform.system() == "Windows":
+            nano_path = r"C:\Program Files\Git\usr\bin\nano.exe"
+            if not os.path.isfile(nano_path):
+                typer.secho(
+                    f"Make sure nano is installed on correct path:{nano_path}",
+                    fg=typer.colors.BRIGHT_RED,
+                )
+                sys.exit()
+            subprocess.call((nano_path, "-v", rpath))
+        else:
+            subprocess.call(("nano", "-v", rpath))
