27dbc0427b77d1b751151763f62bf4969980152d
Anurag-gg <dsanurag520@gmail.com>
2022-05-26 21:42:32 +0530

    new flags

--- chalkit/main.py
+++ chalkit/main.py
@@ -2,31 +2,32 @@ import os
 import json
 import typer
 from appdirs import user_config_dir
-from setup import setup
-from editor import editor
-from rreset import rreset
+from chalkit.setup import setup
+from chalkit.editor import editor
+from chalkit.rreset import rreset
 
 app = typer.Typer()
 config_dir = user_config_dir("chalkit")
 
 
 @app.command()
-def chalkit(reset: bool = False ,view: bool = False):
+def chalkit(reset: bool = False, view: bool = False):
     if reset:
         rreset(config_dir)
-    elif view:
-        pass
     else:
         try:
             f = open(os.path.join(config_dir, "config.json"))
             data = json.load(f)
             path = data["path"]
             f.close()
-            editor(path)
+            if view:
+                editor(path, view_only=True)
+            else:
+                editor(path, view_only=False)
         except (FileNotFoundError, AttributeError, json.decoder.JSONDecodeError):
             setup(config_dir)
         except Exception as e:
             typer.secho(f"Error:{e}", fg=typer.colors.BRIGHT_RED)
 
 
-
+app()
