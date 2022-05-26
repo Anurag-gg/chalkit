deeba8b9eb5029a5d3fd595ce6668ee17911fde0
Anurag-gg <dsanurag520@gmail.com>
2022-05-26 21:42:53 +0530

    bug fix

--- chalkit/rreset.py
+++ chalkit/rreset.py
@@ -1,14 +1,15 @@
 import shutil
 import typer
 
+
 def rreset(path):
     typer.secho("Are you sure you want to reset? (y/n)")
     choice = input()
-    if choice == 'y':
+    if choice == "y":
         try:
             shutil.rmtree(path)
-            typer.secho("Reset success!!" , fg=typer.colors.BRIGHT_GREEN)
+            typer.secho("Reset success!!", fg=typer.colors.BRIGHT_GREEN)
         except:
-            typer.secho("Error: No configurations found" ,fg=typer.colors.BRIGHT_RED)
+            typer.secho("Error: No configurations found", fg=typer.colors.BRIGHT_RED)
     else:
-        typer.secho("Aborting!!!" , fg=typer.colors.BRIGHT_GREEN)
+        typer.secho("Aborting!!!", fg=typer.colors.BRIGHT_GREEN)
